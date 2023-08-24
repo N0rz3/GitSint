6ef9372288e3551d2eae51620b28ff74ef30fdd3
Norze <123885505+N0rz3@users.noreply.github.com>
2023-08-24 14:33:00 +0200

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -6,7 +6,7 @@ banner = f"""{RED}
 -(((---(((----------------------  
  / _] |_   _/' _/| |  \| |_   _| 
 | [/\ | | | `._`.| | | ' | | |   
- \__/_| |_| |___/|_|_|\__| |_|   {PURPLE}{italic("GitSint v2.1 🐙")}{RED}
+ \__/_| |_| |___/|_|_|\__| |_|   {PURPLE}{italic("GitSint v2.1")}{RED} 🐙
      
           {WHITE}BY Norze
   GitHub {RED}OSINT{WHITE} tool made with 💖
@@ -18,7 +18,7 @@ banner2 = f"""{RED}
 -(((---(((----------------------  
  / _] |_   _/' _/| |  \| |_   _| 
 | [/\ | | | `._`.| | | ' | | |   
- \__/_| |_| |___/|_|_|\__| |_|   {PURPLE}{italic("GitSint v2.1 🐙")}{RED}
+ \__/_| |_| |___/|_|_|\__| |_|   {PURPLE}{italic("GitSint v2.1")}{RED} 🐙
      
           {WHITE}BY Norze
   GitHub {RED}OSINT{WHITE} tool made with 💖
