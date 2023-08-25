cf2aa9c6cabbf1bbb169d4bc01d40931f19e090f
Norze <123885505+N0rz3@users.noreply.github.com>
2023-08-25 13:07:16 +0200

    Update Requests.py

--- lib/Requests.py
+++ lib/Requests.py
@@ -5,16 +5,48 @@ class Requests:
                 url: str,
                 headers=None,
                 data=None,
-                params=None):
+                params=None,
+                json=None):
         self.url = url
         self.head = headers
         self.data = data
         self.params = params
+        self.json = json
 
     async def get(self):
         try:
             async with httpx.AsyncClient() as client:
                 requests = await client.get(url=self.url, headers=self.head, params=self.params)
+
+                return requests
+
+        except httpx.HTTPError:
+            return()
+        
+    async def post(self):
+        try:
+            async with httpx.AsyncClient() as client:
+                requests = await client.post(url=self.url, headers=self.head, params=self.params, json=self.json)
+
+                return requests
+            
+        except httpx.HTTPError:
+            return()
+            
+    async def put(self):
+        try:
+            async with httpx.AsyncClient() as client:
+                requests = await client.put(url=self.url, headers=self.head, params=self.params, json=self.json)
+
+                return requests
+            
+        except httpx.HTTPError:
+            return()
+            
+    async def delete(self):
+        try:
+            async with httpx.AsyncClient() as client:
+                requests = await client.delete(url=self.url, headers=self.head, params=self.params)
 
                 return requests
 
