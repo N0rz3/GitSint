f82ab612ede715db6cce3acc6e418cb53a407daa
Norze <123885505+N0rz3@users.noreply.github.com>
2023-08-27 15:01:00 +0200

    Update banner.py

--- lib/banner.py
+++ lib/banner.py
@@ -39,7 +39,7 @@ options:
                         search for an account by email
   -f [FRIENDS], --friends [FRIENDS]
                         search for potential friends by username
-  -l, --light           light mode with option '-e'
   -a [AVATAR], --avatar [AVATAR]
                         download profile picture (avatar) by username
+  -l, --light           light mode with option '-e'
 """
