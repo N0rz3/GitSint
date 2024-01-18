e8a59860b3915acef527efc5fd2511d103ff9f5d
Norze <123885505+N0rz3@users.noreply.github.com>
2024-01-18 20:46:51 +0100

    Update gitsint.py

--- gitsint.py
+++ gitsint.py
@@ -5,4 +5,5 @@ __author__ = "N0rz3"
 if __name__ == "__main__":
     import os; os.system("title GitSint")
     import sys; sys.dont_write_bytecode = True
+    from lib.banner import banner; print(banner)
     from main import check_python_version; check_python_version()
