54e6125004e52f5c828059c5fd0a04de92efbf78
Norze <123885505+N0rz3@users.noreply.github.com>
2024-01-19 20:03:30 +0100

    Delete lib/utils/text.py

--- lib/utils/text.py
+++
@@ -1,9 +0,0 @@
-RED = "\033[31m"
-WHITE = "\033[0m"
-GREEN = "\033[38;2;0;201;87m"
-PURPLE = "\033[38;2;171;130;255m"
-BLACK = "\033[38;2;89;89;89m"
-
-def italic(text):
-    ITALIC = "\033[3m" + text + "\033[0m"
-    return ITALIC
