e1bdc9269814ed91a420db3a16a48e133048e60e
Norze <123885505+N0rz3@users.noreply.github.com>
2026-06-28 19:04:37 +0200

    Refactor main execution block in gitsint.py
    Removed unnecessary os.system call to set terminal title.

--- gitsint.py
+++ gitsint.py
@@ -3,7 +3,6 @@
 __author__ = "N0rz3"
 
 if __name__ == "__main__":
-    import os; os.system("title GitSint")
     import sys; sys.dont_write_bytecode = True
     from lib.utils.banner import banner; print(banner)
     from main import check_python_version; check_python_version()
